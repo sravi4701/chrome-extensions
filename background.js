77adb028f6fb014c68f6d4018693e5a4801fec42
sravi4701 <sravi4701@gmail.com>
2017-07-21 23:44:10 +0530

    add content.js and jquery

--- background.js
+++ background.js
@@ -1,3 +1,12 @@
 chrome.browserAction.onClicked.addListener(function(tab){
-	console.log("working fine");
+	// getting the tab info
+	chrome.tabs.query({active : true, currentWindow : true}, function(tabs){
+		//get active tab
+		var currentTab = tabs[0];
+		// console.log(currentTab.id);
+
+		//send message to the content script
+
+		chrome.tabs.sendMessage(currentTab.id, {"message" : "icon_clicked"});
+	});
 });
