d803608b70756da33dbc2a12abed202938b736d3
Ravi Shankar <sravi4701@gmail.com>
2018-03-29 20:00:10 +0530

    update version 1.1

--- content/content.js
+++ content/content.js
@@ -3,7 +3,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 	if(request.message === "icon_clicked"){
 		var video_id = window.location.href.split("/");
 		video_id = video_id[video_id.length - 1];
-		var getcdn_url = "http://getcdn.hotstar.com/AVS/besc?action=GetCDN&asJson=Y&channel=TABLET&id=" + video_id + 
+		var getcdn_url = "http://agl-intl.hotstar.com/AVS/besc?action=GetCDN&asJson=Y&channel=TABLET&id=" + video_id + 
 		"&type=VOD";
 
 		var isnum = /^\d+$/.test(video_id);
@@ -12,10 +12,11 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 			$.ajax({
 				url : getcdn_url,
 				success : function(result){
+					// console.log(result)
 					// console.log(typeof(result));
-					var responseObj = JSON.parse(result);
-					var src = responseObj.resultObj.src;
-					// console.log(src);
+					// var responseObj = JSON.parse(result);
+					var src = result.resultObj.src;
+					console.log(src);
 					// pass message to background
 					chrome.runtime.sendMessage({"message" : "open_hls", "src" : src});
 				}
